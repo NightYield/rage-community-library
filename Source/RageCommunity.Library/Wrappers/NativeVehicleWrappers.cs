e5beee846bd2e1e620ffee11e95066a0ab156ada
Rich Dunne <richdunne@yahoo.com>
2021-04-10 14:18:10 -0600

    Removed extra ///

--- Source/RageCommunity.Library/Wrappers/NativeVehicleWrappers.cs
+++ Source/RageCommunity.Library/Wrappers/NativeVehicleWrappers.cs
@@ -9,7 +9,7 @@ namespace RageCommunity.Library.Wrappers
         /// <summary>
         /// Causes the given vehicle to honk it's horn for a specified duration in seconds.
         /// </summary>
-        /// /// <param name="heldDown">Whether the horn will be held down or honked normally</param>
+        /// <param name="heldDown">Whether the horn will be held down or honked normally</param>
         public static void StartVehicleHorn(Vehicle vehicle, int duration, bool heldDown, bool forever)
         {
             string mode = !heldDown ? "NORMAL" : "HELDDOWN";
