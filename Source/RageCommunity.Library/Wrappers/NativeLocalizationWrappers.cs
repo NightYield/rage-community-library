8fda8680c7e457ef82b204eb01c9b1c4f2de8ecb
Simon Brozio <Simon.Brozio@gmx.de>
2021-04-11 20:37:50 +0200

    Fix name for get language native.

--- Source/RageCommunity.Library/Wrappers/NativeLocalizationWrappers.cs
+++ Source/RageCommunity.Library/Wrappers/NativeLocalizationWrappers.cs
@@ -10,7 +10,7 @@ namespace RageCommunity.Library.Wrappers
         /// Get the current game language.
         /// </summary>
         /// <returns>A number from 0-12, representing a language. 0 = american. For more languages see <a href="https://alloc8or.re/gta5/nativedb/?n=0x2BDD44CC428A7EAE">Grant Theft Auto V native DB</a>.</returns>
-        public static int DisplayHud()
+        public static int GetGameLanguage()
         {
             return NativeFunction.Natives.x2BDD44CC428A7EAE<int>();
         }
