d0128bad0b7eb04fefefb7ff4b2d3eb7d5ae3f03
Rich Dunne <richdunne@yahoo.com>
2021-04-11 15:39:24 -0600

    Added Marker class
    Added MarkerTypes enum
    Simplified DrawMarker wrapper method
    Added second DrawMarker method to utilize `textureDictionary` and `textureName` parameters

---
+++ Source/RageCommunity.Library/Graphics/Marker.cs
@@ -0,0 +1,114 @@
+﻿using Rage;
+using RageCommunity.Library.Wrappers;
+using System.Drawing;
+
+namespace RageCommunity.Library.Graphics
+{
+    public class Marker : ISpatial
+    {
+        public MarkerTypes MarkerType { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Direction { get; set; }
+        public Vector3 Rotation { get; set; }
+        public Vector3 Scale { get; set; }
+        public Color MarkerColor { get; set; }
+        public bool BobUpAndDown { get; set; }
+        public bool FaceCamera { get; set; }
+        public bool Rotate { get; set; }
+        public string TextureDictionary { get; set; }
+        public string TextureName { get; set; }
+        public bool DrawOnEntities { get; set; }
+
+        public Marker(MarkerTypes type, 
+                    Vector3 position, 
+                    Vector3 direction,
+                    Vector3 rotation,
+                    Vector3 scale,
+                    Color color,
+                    bool bobUpAndDown, 
+                    bool faceCamera, 
+                    bool rotate, 
+                    bool drawOnEntities)
+        {
+            MarkerType = type;
+            Position = position;
+            Direction = direction;
+            Rotation = rotation;
+            Scale = scale;
+            MarkerColor = color;
+            BobUpAndDown = bobUpAndDown;
+            FaceCamera = faceCamera;
+            Rotate = rotate;
+            DrawOnEntities = drawOnEntities;
+        }
+
+        public Marker(MarkerTypes type,
+                    Vector3 position,
+                    Vector3 direction,
+                    Vector3 rotation,
+                    Vector3 scale,
+                    Color color,
+                    bool bobUpAndDown,
+                    bool faceCamera,
+                    bool rotate,
+                    string textureDictionary,
+                    string textureName,
+                    bool drawOnEntities)
+        {
+            MarkerType = type;
+            Position = position;
+            Direction = direction;
+            Rotation = rotation;
+            Scale = scale;
+            MarkerColor = color;
+            BobUpAndDown = bobUpAndDown;
+            FaceCamera = faceCamera;
+            Rotate = rotate;
+            TextureDictionary = textureDictionary;
+            TextureName = textureName;
+            DrawOnEntities = drawOnEntities;
+        }
+
+        public void Draw()
+        {
+            if(!string.IsNullOrWhiteSpace(TextureName) && !string.IsNullOrWhiteSpace(TextureDictionary))
+            {
+                NativeWrappers.DrawMarker(MarkerType, Position, Direction, Rotation, Scale, MarkerColor, BobUpAndDown, FaceCamera, Rotate, TextureDictionary, TextureName, DrawOnEntities);
+            }
+            else
+            {
+                NativeWrappers.DrawMarker(MarkerType, Position, Direction, Rotation, Scale, MarkerColor, BobUpAndDown, FaceCamera, Rotate, DrawOnEntities);
+            }
+        }
+
+        public float DistanceTo(ISpatial target)
+        {
+            return Position.DistanceTo(target);
+        }
+
+        public float DistanceTo(Vector3 target)
+        {
+            return Position.DistanceTo(target);
+        }
+
+        public float DistanceTo2D(Vector3 target)
+        {
+            return Position.DistanceTo2D(target);
+        }
+
+        public float DistanceTo2D(ISpatial target)
+        {
+            return Position.DistanceTo2D(target);
+        }
+
+        public float TravelDistanceTo(Vector3 target)
+        {
+            return Position.TravelDistanceTo(target);
+        }
+
+        public float TravelDistanceTo(ISpatial target)
+        {
+            return Position.TravelDistanceTo(target);
+        }
+    }
+}
--- Source/RageCommunity.Library/Peds/PedTasks.cs
+++ Source/RageCommunity.Library/Peds/PedTasks.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RageCommunity.Library.Peds
+﻿namespace RageCommunity.Library.Peds
 {
 	/// <summary>
 	/// Courtesy of <a href="https://alloc8or.re/gta5/doc/enums/eTaskTypeIndex.txt">Alloc8or.re</a>
