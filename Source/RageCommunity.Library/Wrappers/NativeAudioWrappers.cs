e41e310010cfa61c6b7733333c0c4b0cdfa83e70
Simon Brozio <Simon.Brozio@gmx.de>
2021-04-11 20:36:36 +0200

    Add more native functions.

--- Source/RageCommunity.Library/Wrappers/NativeAudioWrappers.cs
+++ Source/RageCommunity.Library/Wrappers/NativeAudioWrappers.cs
@@ -6,6 +6,12 @@ namespace RageCommunity.Library.Wrappers
 {
     public static partial class NativeWrappers
     {
-        
+        /// <summary>
+        /// Varies the horn sound for a vehicle.
+        /// </summary>
+        public static void SetVehicleHornVariation(Vehicle vehicle, int variation)
+        {
+            NativeFunction.Natives.x0350E7E17BA767D0(vehicle, variation);
+        }
     }
 }
--- Source/RageCommunity.Library/Wrappers/NativeFireWrappers.cs
+++ Source/RageCommunity.Library/Wrappers/NativeFireWrappers.cs
@@ -6,6 +6,15 @@ namespace RageCommunity.Library.Wrappers
 {
     public static partial class NativeWrappers
     {
-        
+        /// <summary>
+        /// Gets the position of the closes fire based on the given search position.
+        /// </summary>
+        /// <returns>
+        /// Returns true when a fire was found, otherwise false.
+        /// </returns>
+        public static Boolean GetClosestFirePosition(out Vector3 foundFirePosition, Vector3 searchPosition)
+        {
+            return NativeFunction.Natives.x352A9F6BCF90081F<Boolean>(out foundFirePosition, searchPosition.X, searchPosition.Y, searchPosition.Z); 
+        }
     }
 }
