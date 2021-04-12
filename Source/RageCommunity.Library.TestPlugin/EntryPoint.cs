407ddc667f2b02cf7b6e624dd811fc5e14d6419d
Simon Brozio <Simon.Brozio@gmx.de>
2021-04-12 22:44:22 +0200

    Add test plugin. Add native defect attribute.

---
+++ Source/RageCommunity.Library.TestPlugin/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+
+[assembly: Rage.Attributes.Plugin("Rage Community Library Test", Description = "A plugin to test the contents of the Rage Community Library.", Author = "NightYield and Contributors")]
+namespace RageCommunity.Library.TestPlugin
+{
+    public class EntryPoint
+    {
+        /// <summary>
+        /// This is the EntryPoint for the RagePluginHook. The plugin has to be kept alive, otherwise console commands will not work.
+        /// </summary>
+        public static void Main()
+        {
+            while (true)
+            {
+                GameFiber.Yield();
+            }
+        }
+    }
+}
--- Source/RageCommunity.Library/Wrappers/NativeAudioWrappers.cs
+++ Source/RageCommunity.Library/Wrappers/NativeAudioWrappers.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Security.Policy;
 using Rage;
 using Rage.Native;
+using RageCommunity.Library.Attributes;
 
 namespace RageCommunity.Library.Wrappers
 {
@@ -9,9 +11,28 @@ namespace RageCommunity.Library.Wrappers
         /// <summary>
         /// Varies the horn sound for a vehicle.
         /// </summary>
+        [DefectNative("Rich", "A System.ArgumentException 'address can not be zero' is being thrown on call.")]
         public static void SetVehicleHornVariation(Vehicle vehicle, int variation)
         {
             NativeFunction.Natives.x0350E7E17BA767D0(vehicle, variation);
+        }
+
+        /// <summary>
+        /// Gets the vehicles default horn.
+        /// </summary>
+        [DefectNative("NightYield", "A System.MissingMethodException is being thrown on call.")]
+        public static Hash GetVehicleDefaultHorn(Vehicle vehicle)
+        {
+            return NativeFunction.Natives.x02165D55000219AC<Hash>(vehicle);
+        }
+
+        /// <summary>
+        /// Gets the vehicles default horn variation.
+        /// </summary>
+        [DefectNative("NightYield", "A System.ArgumentException 'address can not be zero' is being thrown on call.")]
+        public static int GetVehicleDefaultHornVariation(Vehicle vehicle)
+        {
+            return NativeFunction.Natives.xD53F3A29BCE2580E<int>(vehicle);
         }
     }
 }
