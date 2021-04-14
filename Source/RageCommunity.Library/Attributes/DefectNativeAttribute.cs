1f34df87e2b4331fc7e7cf51d6b47348ffdadbe7
Rich Dunne <richdunne@yahoo.com>
2021-04-14 06:48:04 -0600

    Add DefectNativeAttribute

---
+++ Source/RageCommunity.Library/Attributes/DefectNativeAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace RageCommunity.Library.Attributes
+{
+    public class DefectNativeAttribute : System.Attribute
+    {
+        /// <summary>
+        /// The developer who found the defect.
+        /// </summary>
+        public string Developer { get; set; }
+
+        /// <summary>
+        /// A description of the defect.
+        /// </summary>
+        public string Description { get; set; }
+
+        public DefectNativeAttribute(string developer, string description)
+        {
+            Developer = developer;
+            Description = description;
+        }
+    }
+}
