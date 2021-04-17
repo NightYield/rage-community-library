17e902d1522ce51ef431296db6bc50eecbefd974
Rich Dunne <richdunne@yahoo.com>
2021-04-17 10:20:32 -0600

    Added bone enums

---
+++ Source/RageCommunity.Library/Weapons/WeaponBone.cs
@@ -0,0 +1,35 @@
+﻿namespace RageCommunity.Library.Weapons
+{
+    public enum WeaponBone
+    {
+        gun_root = 0,
+        gun_gripr = 1,
+        gun_gripl = 2,
+        gun_muzzle = 3,
+        gun_vfx_eject = 4,
+        gun_magazine = 5,
+        gun_ammo = 6,
+        gun_vfx_projtrail = 7,
+        gun_barrels = 8,
+        WAPClip = 9,
+        WAPClip_2 = 10,
+        WAPScop = 11,
+        WAPScop_2 = 12,
+        WAPGrip = 13,
+        WAPGrip_2 = 14,
+        WAPSupp = 15,
+        WAPSupp_2 = 16,
+        WAPFlsh = 17,
+        WAPFlsh_2 = 18,
+        WAPStck = 19,
+        WAPSeWp = 20,
+        WAPLasr = 21,
+        WAPLasr_2 = 22,
+        WAPFlshLasr = 23,
+        WAPFlshLasr_2 = 24,
+        NM_Butt_Marker = 25,
+        Gun_GripR = 26,
+        Gun_Nuzzle = 27,
+        gun_drum = 28
+    }
+}
