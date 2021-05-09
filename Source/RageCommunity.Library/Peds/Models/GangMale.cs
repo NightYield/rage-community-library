4daf76345d993f0c52ee8fbf56d4c6e49c791048
Rich Dunne <richdunne@yahoo.com>
2021-05-09 10:44:15 -0600

    Added ped model enums

---
+++ Source/RageCommunity.Library/Peds/Models/GangMale.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RageCommunity.Library.Peds.Models
+{
+    public enum GangMale : Int64
+    {
+        g_m_importexport_01 = 3164785898,
+        g_m_m_armboss_01 = -236444766,
+        g_m_m_armgoon_01 = -39239064,
+        g_m_m_armlieut_01 = -412008429,
+        g_m_m_chemwork_01 = -166363761,
+        g_m_m_chiboss_01 = -1176698112,
+        g_m_m_chicold_01 = 275618457,
+        g_m_m_chigoon_01 = 2119136831,
+        g_m_m_chigoon_02 = -9308122,
+        g_m_m_korboss_01 = 891945583,
+        g_m_m_mexboss_01 = 1466037421,
+        g_m_m_mexboss_02 = 1226102803,
+        g_m_y_armgoon_02 = -984709238,
+        g_m_y_azteca_01 = 1752208920,
+        g_m_y_ballaeast_01 = -198252413,
+        g_m_y_ballaorig_01 = 588969535,
+        g_m_y_ballasout_01 = 599294057,
+        g_m_y_famca_01 = -398748745,
+        g_m_y_famdnf_01 = -613248456,
+        g_m_y_famfor_01 = -2077218039,
+        g_m_y_korean_01 = 611648169,
+        g_m_y_korean_02 = -1880237687,
+        g_m_y_korlieut_01 = 2093736314,
+        g_m_y_lost_01 = 1330042375,
+        g_m_y_lost_02 = 1032073858,
+        g_m_y_lost_03 = 850468060,
+        g_m_y_mexgang_01 = -1109568186,
+        g_m_y_mexgoon_01 = 653210662,
+        g_m_y_mexgoon_02 = 832784782,
+        g_m_y_mexgoon_03 = -1773333796,
+        g_m_y_pologoon_01 = 1329576454,
+        g_m_y_pologoon_02 = -1561829034,
+        g_m_y_salvaboss_01 = -1872961334,
+        g_m_y_salvagoon_01 = 663522487,
+        g_m_y_salvagoon_02 = 846439045,
+        g_m_y_salvagoon_03 = 62440720,
+        g_m_y_strpunk_01 = -48477765,
+        g_m_y_strpunk_02 = 228715206,
+        g_m_m_casrn_01 = 1020431539
+    }
+}
---
+++ Source/RageCommunity.Library/Peds/Models/Multiplayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace RageCommunity.Library.Peds.Models
+{
+    public enum Multiplayer : Int64
+    {
+        mp_f_bennymech_01 = 2139205821,
+        mp_f_boatstaff_01 = -933295480,
+        mp_f_cardesign_01 = 606876839,
+        mp_f_chbar_01 = 3287737221,
+        mp_f_cocaine_01 = 1264941816,
+        mp_f_counterfeit_01 = 3079205365,
+        mp_f_deadhooker = 1943971979,
+        mp_f_execpa_01 = 1126998116,
+        mp_f_execpa_02 = 1500695792,
+        mp_f_forgery_01 = 2014985464,
+        mp_f_freemode_01 = -1667301416,
+        mp_f_helistaff_01 = 431423238,
+        mp_f_meth_01 = 3534913217,
+        mp_f_misty_01 = -785842275,
+        mp_f_stripperlite = 695248020,
+        mp_f_weed_01 = 2992993187,
+        mp_g_m_pros_01 = 1822283721,
+        mp_m_avongoon = 2618542997,
+        mp_m_boatstaff_01 = 3361671816,
+        mp_m_bogdangoon = 1297520375,
+        mp_m_claude_01 = -1057787465,
+        mp_m_cocaine_01 = 1456705429,
+        mp_m_counterfeit_01 = 2555758964,
+        mp_m_exarmy_01 = 1161072059,
+        mp_m_execpa_01 = 1048844220,
+        mp_m_famdd_01 = 866411749,
+        mp_m_fibsec_01 = 1558115333,
+        mp_m_forgery_01 = 1631482011,
+        mp_m_freemode_01 = 1885233650,
+        mp_m_g_vagfun_01 = 3299219389,
+        mp_m_marston_01 = 943915367,
+        mp_m_meth_01 = 3988008767,
+        mp_m_niko_01 = -287649847,
+        mp_m_securoguard_01 = 3660355662,
+        mp_m_shopkeep_01 = 416176080,
+        mp_m_waremech_01 = 4154933561,
+        mp_m_weapexp_01 = 921328393,
+        mp_m_weapwork_01 = 1099321454,
+        mp_m_weed_01 = 2441008217,
+        mp_s_m_armoured_01 = -839953400
+    }
+}
