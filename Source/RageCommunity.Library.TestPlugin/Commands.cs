4e8c58db6d7a62a675021341ee856616633b8fb9
Rich Dunne <richdunne@yahoo.com>
2021-05-09 10:44:49 -0600

    Added command to test spawning models from enums

--- Source/RageCommunity.Library.TestPlugin/Commands.cs
+++ Source/RageCommunity.Library.TestPlugin/Commands.cs
@@ -2,6 +2,9 @@
 using Rage.Attributes;
 using RageCommunity.Library.Extensions;
 using RageCommunity.Library.Wrappers;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace RageCommunity.Library.TestPlugin
 {
@@ -21,6 +24,37 @@ namespace RageCommunity.Library.TestPlugin
         public static void Command_GetZoneNameForCurrentLocation()
         {
             Game.LogTrivial($"The name of the current zone is: {Game.LocalPlayer.Character.Position.GetZoneName()}");
+        }
+
+        [ConsoleCommand("Rage Community Library spawn enum models")]
+        public static void Command_SpawnEnumModels()
+        {
+            foreach (Peds.Models.StoryScenarioMale model in (Peds.Models.StoryScenarioMale[])Enum.GetValues(typeof(Peds.Models.StoryScenarioMale)))
+            {
+                Game.LogTrivial($"Model: {model} | Hash: {(uint)model}");
+                try
+                {
+                    var ped = new Ped(model.ToString(), Game.LocalPlayer.Character.GetOffsetPositionFront(5), Game.LocalPlayer.Character.Heading);
+                    Game.LogTrivial($"Ped hash: {ped.Model.Hash}");
+                    ped.Delete();
+                }
+                catch
+                {
+                    Game.LogTrivial($"==================== MODEL NAME ERROR FOR ABOVE MODEL ====================");
+                }
+                
+
+                try
+                {
+                    var ped = new Ped((uint)model, Game.LocalPlayer.Character.GetOffsetPositionFront(5), Game.LocalPlayer.Character.Heading);
+                    ped.Delete();
+                }
+                catch
+                {
+                    Game.LogTrivial($"==================== HASH ERROR FOR ABOVE MODEL ====================");
+                }
+
+            }
         }
     }
 }
