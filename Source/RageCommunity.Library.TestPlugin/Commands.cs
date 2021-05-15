e4c7827f56fe5c7a34aaa40a5d7d8093faf6a3bc
Rich Dunne <richdunne@yahoo.com>
2021-05-15 12:09:24 -0600

    Added commands to spawn ped by model enum

--- Source/RageCommunity.Library.TestPlugin/Commands.cs
+++ Source/RageCommunity.Library.TestPlugin/Commands.cs
@@ -1,5 +1,6 @@
 ﻿using Rage;
 using Rage.Attributes;
+using Rage.ConsoleCommands.AutoCompleters;
 using RageCommunity.Library.Extensions;
 using RageCommunity.Library.Wrappers;
 using System;
@@ -26,34 +27,122 @@ namespace RageCommunity.Library.TestPlugin
             Game.LogTrivial($"The name of the current zone is: {Game.LocalPlayer.Character.Position.GetZoneName()}");
         }
 
-        [ConsoleCommand("Rage Community Library spawn enum models")]
-        public static void Command_SpawnEnumModels()
+        [ConsoleCommand("Rage Community Library Delete Entity")]
+        public static void Command_DeleteEntity([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandAutoCompleterEntity))] Entity entity)
         {
-            foreach (Peds.Models.StoryScenarioMale model in (Peds.Models.StoryScenarioMale[])Enum.GetValues(typeof(Peds.Models.StoryScenarioMale)))
+            if (entity)
             {
-                Game.LogTrivial($"Model: {model} | Hash: {(uint)model}");
-                try
-                {
-                    var ped = new Ped(model.ToString(), Game.LocalPlayer.Character.GetOffsetPositionFront(5), Game.LocalPlayer.Character.Heading);
-                    Game.LogTrivial($"Ped hash: {ped.Model.Hash}");
-                    ped.Delete();
-                }
-                catch
-                {
-                    Game.LogTrivial($"==================== MODEL NAME ERROR FOR ABOVE MODEL ====================");
-                }
-                
+                entity.Delete();
+            }
+        }
 
-                try
-                {
-                    var ped = new Ped((uint)model, Game.LocalPlayer.Character.GetOffsetPositionFront(5), Game.LocalPlayer.Character.Heading);
-                    ped.Delete();
-                }
-                catch
-                {
-                    Game.LogTrivial($"==================== HASH ERROR FOR ABOVE MODEL ====================");
-                }
+        [ConsoleCommand("Rage Community Library Delete Vehicle")]
+        public static void Command_DeleteVehicle([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandAutoCompleterVehicle))] Vehicle vehicle)
+        {
+            if (vehicle)
+            {
+                vehicle.Delete();
+            }
+        }
 
+        [ConsoleCommand("Rage Community Library Delete Ped")]
+        public static void Command_DeletePed([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandAutoCompleterPed))] Ped ped)
+        {
+            if(ped)
+            {
+                ped.Delete();
+            }
+        }
+
+        [ConsoleCommand("Rage Community Library spawn AmbientFemale model")]
+        public static void Command_SpawnAmbientFemaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.AmbientFemale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn AmbientMale model")]
+        public static void Command_SpawnAmbientMaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.AmbientMale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn Animal model")]
+        public static void Command_SpawnAnimalModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.Animal model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn Cutscene model")]
+        public static void Command_SpawnCutsceneModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.Cutscene model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn GangFemale model")]
+        public static void Command_SpawnGangFemaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.GangFemale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn GangMale model")]
+        public static void Command_SpawnGangMaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.GangMale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn Multiplayer model")]
+        public static void Command_SpawnMultiplayerModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.Multiplayer model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn ScenarioFemale model")]
+        public static void Command_SpawnScenarioFemaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.ScenarioFemale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn ScenarioMale model")]
+        public static void Command_SpawnScenarioMaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.ScenarioMale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn Story model")]
+        public static void Command_SpawnStoryModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.Story model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn StoryScenarioFemale model")]
+        public static void Command_SpawnStoryScenarioFemaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.StoryScenarioFemale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        [ConsoleCommand("Rage Community Library spawn StoryScenarioMale model")]
+        public static void Command_SpawnStoryScenarioMaleModel([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum))] Peds.Models.StoryScenarioMale model)
+        {
+            SpawnPedModel(model);
+        }
+
+        private static void SpawnPedModel<T>(T model)
+        {
+            Type type = model.GetType();
+            if (!type.IsEnum)
+            {
+                Game.LogTrivial($"Type is not an enum.");
+                return;
+            }
+
+            try
+            {
+                Game.LogTrivial($"Spawning {model}");
+                new Ped(model.ToString(), Game.LocalPlayer.Character.GetOffsetPositionFront(5), Game.LocalPlayer.Character.Heading);
+            }
+            catch
+            {
+                Game.LogTrivial($"Error spawning {model}");
             }
         }
     }
